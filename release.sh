7604f05877f17c2a28a5012bd23a22831dea210f
Raj Patil <rajp152k@gmail.com>
2025-06-16 13:39:53 +0530

    vscode client removed

--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-poetry build && poetry publish
-curl https://github.com/sakuraiyuta/hyuga/blob/master/README.md | \
-	grep -Eo '<img src="[^"]+"' | grep camo | grep -Eo 'https[^"]+' | \
-	xargs -I {} curl -w "\n" -s -X PURGE {}
